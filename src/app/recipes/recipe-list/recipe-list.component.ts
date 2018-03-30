f90ed5c7cf9801e97ec3d2d699742694ad6939c1
SA951398 <santosh.p59@wipro.com>
2018-03-30 15:58:06 +0530

    adding features

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes : Recipe[] = [ 
+    new Recipe('A Test Recipe', 'Testing Recipe Desc', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Second Test Recipe', 'Second Recipe Description', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXP3KTHHbSTI9kj6Yv_oiK_bzZWM7pOmBAdWuHeKgXTJeLl7ki'),
+    new Recipe('Third Test Recipe', 'Third testing recipe description', 'https://www.brandcrowd.com/gallery/brands/pictures/picture13963851376591.png')];
 
   constructor() { }
 
